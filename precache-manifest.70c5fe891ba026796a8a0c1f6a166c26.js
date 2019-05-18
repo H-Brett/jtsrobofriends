9b23b7fa54812c1446002e0ab226d9c552a0538a
Brett H <Horst.Brett@gmail.com>
2019-05-18 15:41:22 -0600

    Updates

---
+++ precache-manifest.70c5fe891ba026796a8a0c1f6a166c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c05b98d8c99c3e321c9d41ad548b84",
+    "url": "/jtsrobofriends/index.html"
+  },
+  {
+    "revision": "4571fd4802c9783376b0",
+    "url": "/jtsrobofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d2ea411107027469e5f1",
+    "url": "/jtsrobofriends/static/css/main.f888c0a3.chunk.css"
+  },
+  {
+    "revision": "4571fd4802c9783376b0",
+    "url": "/jtsrobofriends/static/js/2.096916fd.chunk.js"
+  },
+  {
+    "revision": "d2ea411107027469e5f1",
+    "url": "/jtsrobofriends/static/js/main.191357c5.chunk.js"
+  },
+  {
+    "revision": "54ee4be6cb70e81ea0a2",
+    "url": "/jtsrobofriends/static/js/runtime~main.50ce020e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/jtsrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
